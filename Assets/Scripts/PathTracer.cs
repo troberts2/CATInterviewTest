89f850c453c0d25edbddaa15e0ccb0a001776384
troberts2 <troberts2@mail.bradley.edu>
2025-04-12 18:22:25 -0500

    added speed change and reverse functionality

--- Assets/Scripts/PathTracer.cs
+++ Assets/Scripts/PathTracer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PathTracer : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class PathTracer : MonoBehaviour
     private Transform[] _currentPointTransforms;
 
     private int _currentPolygonIndex = 0;
+
+    private Vector2 _nextPoint;
+    private Vector2 _currentPosition;
 
     [Header("Circle Settings")]
     private Vector2 _centerPoint = new Vector2(0, 2); //arbitrary middle of the other shapes value
@@ -25,6 +29,10 @@ public class PathTracer : MonoBehaviour
     [SerializeField] private float _traceSpeed = 2f;
     
     private bool _isTracing = false;
+
+    [SerializeField] private Slider _traceSpeedSlider;
+    private bool _isReversed = false;
+    [SerializeField] private Toggle _reverseToggle;
 
     private enum ShapeType
     {
@@ -55,6 +63,45 @@ public class PathTracer : MonoBehaviour
         }
     }
 
+    #region Trace UI
+
+    public void OnSpeedSliderChanged()
+    {
+        if(_traceSpeedSlider != null)
+        {
+            _traceSpeed = _traceSpeedSlider.value;
+        }
+    }
+
+    public void OnReverseToggle()
+    {
+        if(_reverseToggle != null)
+        {
+            _isReversed = !_isReversed;
+
+            //if tracing polygons
+            if(_currentPointTransforms != null)
+            {
+                if (_isReversed)
+                {
+                    _currentPolygonIndex--;
+                    if (_currentPolygonIndex < 0)
+                        _currentPolygonIndex = _currentPointTransforms.Length - 1;
+                }
+                else
+                {
+                    _currentPolygonIndex++;
+                    if(_currentPolygonIndex > _currentPointTransforms.Length - 1)
+                        _currentPolygonIndex = 0;
+                }
+
+                _nextPoint = _currentPointTransforms[_currentPolygonIndex].position;
+            }
+        }
+    }
+
+    #endregion
+
     #region Trace Functionality
 
     private void TracePath(ShapeType shape)
@@ -74,28 +121,46 @@ public class PathTracer : MonoBehaviour
         }
     }
 
+
     private void TracePolygon()
     {
-        Vector2 nextPoint = _currentPointTransforms[_currentPolygonIndex].position;
-        Vector2 currentPosition = _sphere.position;
+
+        _nextPoint = _currentPointTransforms[_currentPolygonIndex].position;
+        _currentPosition = _sphere.position;
 
         //move target sphere toward next point
-        _sphere.position = Vector2.MoveTowards(currentPosition, nextPoint, _traceSpeed * Time.deltaTime);
+        _sphere.position = Vector2.MoveTowards(_currentPosition, _nextPoint, _traceSpeed * Time.deltaTime);
 
         //check if we've reached next point
-        if(Vector2.Distance(currentPosition, nextPoint) < 0.01f)
+        if(Vector2.Distance(_currentPosition, _nextPoint) < 0.01f)
         {
-            _currentPolygonIndex++;
+            if(!_isReversed)
+            {
+                _currentPolygonIndex++;
 
-            //reset path before going out of bounds
-            if(_currentPolygonIndex > _currentPointTransforms.Length - 1)
-                _currentPolygonIndex = 0;
+                //reset path before going out of bounds
+                if (_currentPolygonIndex > _currentPointTransforms.Length - 1)
+                    _currentPolygonIndex = 0;
+            }
+            else
+            {
+                _currentPolygonIndex--;
+
+                //reset path before going out of bounds
+                if (_currentPolygonIndex < 0)
+                    _currentPolygonIndex = _currentPointTransforms.Length - 1;
+            }
+            
         }
     }
 
     private void TraceCircle()
     {
-        _angle += _traceSpeed * Time.deltaTime;
+        if (!_isReversed)
+            _angle += _traceSpeed * Time.deltaTime;
+        else
+            _angle -= _traceSpeed * Time.deltaTime;
+
         float x = Mathf.Cos(_angle) * _circleRadius;
         float y = Mathf.Sin(_angle) * _circleRadius;
         _sphere.position = new Vector2(_centerPoint.x + x, _centerPoint.y - y);
